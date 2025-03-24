239b2475ccbf9cfc10003cc71689be747a41700f
Parishlesh <parishlesh@rosmertatech.co.in>
2025-03-24 17:12:27 +0530

    message field need to update more

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,35 +1,57 @@
-import React, { useEffect } from 'react'
-import { useChatStore } from '../store/useChatStore'
-import ChatHeader from './chatHeader'
-import MessageInput from './messageInput'
-import MessageSkeleton from './skeleton/MessageSkeleton'
+import React, { useEffect } from 'react';
+import { useChatStore } from '../store/useChatStore';
+import { useAuth } from '../store/useAuth';
+import ChatHeader from './chatHeader';
+import MessageInput from './messageInput';
+import MessageSkeleton from './skeleton/MessageSkeleton';
 
 const ChatContainer = () => {
-    const {messages, getMessages, isMessagesLoading, selectedUser}=useChatStore()
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+  const { authUser } = useAuth();
 
-    useEffect(() => {
-        if (selectedUser?._id) {
-            getMessages(selectedUser._id);
-        }
-    }, [selectedUser?._id, getMessages]);
-    
+  useEffect(() => {
+    if (selectedUser?._id) {
+      getMessages(selectedUser._id);
+    }
+  }, [selectedUser?._id, getMessages]);
 
-if(isMessagesLoading){
-    return
-     <div className='flex-1 flex flex-col overflow-auto'><ChatHeader/>
-    <MessageSkeleton/>
-    <messageInput/>
-    </div>
-}
-
+  if (isMessagesLoading) {
+    return (
+      <div className='flex-1 flex flex-col overflow-auto'>
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
 
   return (
     <div>
-    <ChatHeader/>
-<p>meaages</p>
-    <MessageInput/>
+      <ChatHeader />
+      <div>
+        {messages?.length > 0 ? (
+          messages.map((message) => (
+            <div
+              key={message._id}
+              className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            >
+              <div className="chat-image avatar">
+                <div className="size-10 rounded-full border">
+                  <img
+                    src={message.senderId === authUser._id ? authUser.profilePic || "avatar.png" : selectedUser?.profilePic || "avatar.png"}
+                    alt="profilePic"
+                  />
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center text-gray-500">No messages yet</p>
+        )}
+      </div>
+      <MessageInput />
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;
