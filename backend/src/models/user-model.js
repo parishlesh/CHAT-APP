fc13e236a7c0f9be6151fb2587d6aca7d65adfad
parishlesh <parishleshfulvanshi@gmail.com>
2025-05-22 11:11:54 +0530

    about section in database

--- backend/src/models/user-model.js
+++ backend/src/models/user-model.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema(
             default: "",
 
         },
+        about: {
+            type: String,
+            default: "No bio available.",
+        },
     },
     {
         timestamps: true
