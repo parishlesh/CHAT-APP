4cd190472358a163ab6113f8d759a5429c38af9b
parishlesh <parishleshfulvanshi@gmail.com>
2025-05-22 11:13:37 +0530

    new route

--- backend/src/routes/message-route.js
+++ backend/src/routes/message-route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protectRoute from "../middleware/auth-middleware.js";
-import {getUsersForSidebar, getMessage, sendMessage} from "../controllers/message-controller.js";
+import {getUsersForSidebar, getMessage, sendMessage, getchats} from "../controllers/message-controller.js";
 
 const router = express.Router()
 
 router.get('/users', protectRoute, getUsersForSidebar)
+router.get("/conversations", protectRoute, getchats);
 router.get("/:id", protectRoute, getMessage)
 
 router.post("/send/:id", protectRoute, sendMessage)
