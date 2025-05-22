01a6c13948e824e6e154b8392226b09c3e0cafee
parishlesh <parishleshfulvanshi@gmail.com>
2025-05-22 11:22:40 +0530

    frontend change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,22 +12,21 @@ import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 import { useThemeStore } from './store/useThemeStore'
 import Button from './pages/Button'
+import Sidebar from './components/Sidebar'
 
 
 const App = () => {
-  const {authUser, checkAuth, isCheckingAuth} =useAuth()
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} =useAuth()
    
   const {theme}=  useThemeStore() 
 
+  console.log({onlineUsers});
+
   useEffect(() => {
     checkAuth();
-  }, [checkAuth]);
-
-  useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
-  }, [theme]);
+  }, [checkAuth, theme]);
 
-  console.log("Current Theme:", theme);
   
   if (isCheckingAuth && !authUser)
     return (
@@ -37,20 +36,30 @@ const App = () => {
     );
 
   return (
-<div data-theme={theme}>
-  <Navbar />
-  <Routes>
-    <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
-    <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/" />} />
-    <Route path="/login" element={!authUser ? <Login />: <Navigate to="/" />} />
-    <Route path="/settings" element={<Settings />} />
-    <Route path="/profile" element={authUser ? <Profile />: <Navigate to="/login" />} />
-    <Route path="/button" element={<Button />} />    
+    <div className="flex h-screen w-full" data-theme={theme}>
 
-  </Routes>
-  <Toaster/>
-</div>
+    <div className="w-[20%] bg-gray-100 border-r">
+      <Navbar />
+    </div>
 
+    <div className="w-[60%] overflow-y-auto">
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
+        <Route path="/button" element={<Button />} />
+      </Routes>
+    </div>
+
+  
+    <div className="w-[20%] bg-gray-100 border-l">
+      <Sidebar />
+    </div>
+
+    <Toaster />
+  </div>
   )
 }
 
